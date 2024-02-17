23f1e606e6c37348b500ed25c3d612a7c99d8ed8
BOUNOU <djeey.osis@gmail.com>
2024-02-17 09:37:29 +0100

    portfolio perso

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+/** @format */
+
+import React from 'react';
+
+import styles from './Contact.module.css';
+import { getImageUrl } from '../../utils';
+
+export const Contact = () => {
+  return (
+    <footer id='contact' className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl('contact/emailIcon.png')} alt='Email icon' />
+          <a href='mailto:bounou.oussama@gmail.com'>bounou.oussama@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl('contact/linkedinIcon.png')}
+            alt='LinkedIn icon'
+          />
+          <a href='https://www.linkedin.com/in/oussamabounou/' target='_blank'>
+            linkedin.com/oussambounou
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl('contact/githubIcon.png')} alt='Github icon' />
+          <a href='https://www.github.com/Osis6' target='_blank'>
+            github.com/oussamabounou
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
