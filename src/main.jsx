275b4db0e4e254e7f961a9528544193054ac5326
BOUNOU <djeey.osis@gmail.com>
2024-02-22 22:18:11 +0100

    finalisation

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+/** @format */
 
-import "@fontsource/outfit";
-import "@fontsource/roboto";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import '@fontsource/outfit';
+import '@fontsource/roboto';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
